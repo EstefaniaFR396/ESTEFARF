2356007f79532250d4e091f917384a87ce9a6ced
EstefaniaFR396 <estefania.ramos.florez@gmail.com>
2026-08-20 17:20:56 -0400

    Create N

---
+++ POOConcept.git/backend/Date.cs
@@ -0,0 +1,45 @@
+﻿namespace backend;
+
+
+public class Date
+{
+    // fields
+    private int year;
+    private int month;
+    private int day;
+
+    // constructor  sin parametros
+
+    public Date()
+
+     {
+        year = 1900;
+        month = 1;
+        day = 1;
+    }
+
+    // con paramentros
+    public Date(int year, int month, int day)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+    // properties
+
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int Day { get; set; }
+
+    // public methods    
+    public override string ToString()
+    {
+        return $"{year:D4}/{month:D2}/{day:D2}";
+    }
+
+    //private methods
+
+
+
+
+}
---
+++ POOConcept.git/frontend/Program.cs
@@ -0,0 +1,11 @@
+﻿using backend;
+
+var date1 = new Date();
+
+var date2 = new Date(); //1900/01/01
+var date3 = new Date(1974, 9, 23);
+
+Console.WriteLine(date1.ToString());
+Console.WriteLine(date2.ToString());
+Console.WriteLine(date3.ToString()); 
+
